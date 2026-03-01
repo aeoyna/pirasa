03c12660e194c2c1318c112d256676de7efa0936
aeoyna <c1304558st.kanazawa@gmail.com>
2026-03-01 18:34:12 +0900

    feat: disable context menu globally

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Disable right-click globally
+window.addEventListener('contextmenu', (e) => e.preventDefault());
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
